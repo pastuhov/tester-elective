c3330a2140f2b73830f0e5ecc65a808cbd4c1898
Pastukhov Kirill <kirill@pastukhov.su>
2017-04-19 22:01:13 +0500

    fix namespace

--- tests/models/ContactFormTest.php
+++ tests/models/ContactFormTest.php
@@ -1,13 +1,14 @@
 <?php
 namespace pastuhov\testerelective\tests\models;
 
+use pastuhov\testerelective\models\ContactForm;
 use PHPUnit\Framework\TestCase;
 
 class ContactFormTest extends TestCase
 {
 	public function testCreateObject()
 	{
-		$foo = new \pastukhov\testerelective\models\ContactForm();
+		$foo = new ContactForm();
 		echo 'ok';
 	}
 }
